27966a4924d9563970edfe7b519653877509f01c
egg3d <shina.tmu@gmail.com>
2025-02-23 20:19:42 +0900

    big rank ui

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-FIT-1.0";
 const contentToCache = [
-    "Build/docs.loader.js",
-    "Build/docs.framework.js",
-    "Build/docs.data",
-    "Build/docs.wasm",
+    "Build/3c0f853f17e03fdaa6dd4ed860004e67.loader.js",
+    "Build/1ab213b6743ca9a8cc0e41192190b006.framework.js",
+    "Build/f83aea4229cac8c269f86e534f36321f.data",
+    "Build/4545b341c0fec5d9dc8eaa6ab78efa5a.wasm",
     "TemplateData/style.css"
 
 ];
